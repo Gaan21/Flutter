0fff8c49b2a78f759dd9ab5beb74a04bcffb1ac1
Luciano <luciano--15@hotmail.com>
2023-07-10 11:59:48 +0200

    Añadidas varias PageView

---
+++ tok_tik/lib/presentation/widgets/shared/video_scrollable_view.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:tok_tik/domain/entities/video_post.dart';
+
+class VideoScrollableView extends StatelessWidget {
+  const VideoScrollableView({super.key, required this.videos});
+
+  final List<VideoPost> videos;
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      scrollDirection: Axis.vertical,
+      physics: const BouncingScrollPhysics(),
+      children: [
+        Container(
+          color: Colors.red,
+        ),
+        Container(
+          color: Colors.blue,
+        ),
+        Container(
+          color: Colors.yellow,
+        ),
+      ],
+    );
+  }
+}
