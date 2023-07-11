83ea3addefda11dc5d2171e46557b06bef776063
Luciano <luciano--15@hotmail.com>
2023-07-11 18:57:13 +0200

    Pantalla de carga funcionando

--- tok_tik/lib/shared/data/local_video_posts.dart
+++ tok_tik/lib/shared/data/local_video_posts.dart
@@ -2,7 +2,7 @@ List<Map<String, dynamic>> videoPosts = [
   {
     'name': 'Subiendo escaleras automáticas',
     'videoUrl': 'assets/videos/1.mp4',
-    'likes': 23230,
+    'likes': 23235,
     'views': 1523,
   },
   {
