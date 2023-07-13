96178908ab066884279f95163f775774b6e3b590
Gaan21 <90606314+Gaan21@users.noreply.github.com>
2023-07-13 16:12:32 +0200

    prueba final

--- widgets_app/lib/presentation/screens/buttons/buttons_screen.dart
+++ widgets_app/lib/presentation/screens/buttons/buttons_screen.dart
@@ -109,7 +109,7 @@ class CustomButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final ColorScheme colors = Theme.of(context).colorScheme;
-    //Pruebasss
+    //Pruebas
 
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
