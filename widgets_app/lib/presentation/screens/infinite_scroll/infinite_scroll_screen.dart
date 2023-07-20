4cf244dd064e9bc31d18da378f29b66171165e40
Gaan21 <90606314+Gaan21@users.noreply.github.com>
2023-07-20 17:32:12 +0200

    pequeña refactorización y scroll automático añadido

--- widgets_app/lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
+++ widgets_app/lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
@@ -49,11 +50,39 @@ class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
     if (!isMounted) return;
 
     setState(() {});
+    moveScrollToBottom();
+  }
+
+  Future<void> onRefresh() async {
+    isLoading = true;
+    setState(() {});
+
+    await Future.delayed(const Duration(seconds: 2));
+    if (!isMounted) return;
+
+    final lastId = imagesIds.last;
+    isLoading = false;
+    imagesIds.clear();
+    imagesIds.add(lastId);
+
+    addFiveImages();
+    setState(() {});
   }
 
   void addFiveImages() {
     final lastId = imagesIds.last;
     imagesIds.addAll([1, 2, 3, 4, 5].map((e) => lastId + e));
+  }
+
+  void moveScrollToBottom() {
+    if (scrollController.position.pixels + 100 <=
+        scrollController.position.maxScrollExtent) return;
+
+    scrollController.animateTo(
+      scrollController.position.pixels + 120,
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.fastOutSlowIn,
+    );
   }
 
   @override
@@ -64,25 +93,55 @@ class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
         context: context,
         removeTop: true,
         removeBottom: true,
-        child: ListView.builder(
-          controller: scrollController,
-          itemCount: imagesIds.length,
-          itemBuilder: (context, index) {
-            return FadeInImage(
-              width: double.infinity,
-              height: 300,
-              fit: BoxFit.cover,
-              placeholder: const AssetImage("assets/images/jar-loading.gif"),
-              image: NetworkImage(
-                  "https://picsum.photos/id/${imagesIds[index]}/500/300"),
-            );
-          },
+        child: RefreshIndicator(
+          onRefresh: onRefresh,
+          edgeOffset: 10,
+          strokeWidth: 2,
+          child: _ListViewImagenes(
+              scrollController: scrollController, imagesIds: imagesIds),
         ),
       ),
+      //Para colocar el boton:
       floatingActionButton: FloatingActionButton(
         onPressed: () => context.pop(),
-        child: const Icon(Icons.arrow_back_ios_new),
+        child: isLoading == true
+            ? SpinPerfect(
+                infinite: true,
+                child: const Icon(Icons.refresh_rounded),
+              )
+            : FadeIn(
+                child: const Icon(Icons.arrow_back_ios_new_rounded),
+              ),
       ),
+    );
+  }
+}
+
+class _ListViewImagenes extends StatelessWidget {
+  const _ListViewImagenes({
+    super.key,
+    required this.scrollController,
+    required this.imagesIds,
+  });
+
+  final ScrollController scrollController;
+  final List<int> imagesIds;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      controller: scrollController,
+      itemCount: imagesIds.length,
+      itemBuilder: (context, index) {
+        return FadeInImage(
+          width: double.infinity,
+          height: 300,
+          fit: BoxFit.cover,
+          placeholder: const AssetImage("assets/images/jar-loading.gif"),
+          image: NetworkImage(
+              "https://picsum.photos/id/${imagesIds[index]}/500/300"),
+        );
+      },
     );
   }
 }
